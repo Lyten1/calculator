28e42a60325494e247a165849ce9a4bfca4387d7
Anhelina0224 <63913185+Anhelina0224@users.noreply.github.com>
2020-12-24 15:05:19 +0200

    Interface

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,33 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+   /* connect(ui->pushButton_0,SIGNAl(clicked()),this,SLOT(digits_numbers());
+    connect(ui->pushButton_1,SIGNAl(clicked()),this,SLOT(digits_numbers());
+    connect(ui->pushButton_2,SIGNAl(clicked()),this,SLOT(digits_numbers());
+    connect(ui->pushButton_3,SIGNAl(clicked()),this,SLOT(digits_numbers());
+    connect(ui->pushButton_4,SIGNAl(clicked()),this,SLOT(digits_numbers());
+    connect(ui->pushButton_5,SIGNAl(clicked()),this,SLOT(digits_numbers());
+    connect(ui->pushButton_6,SIGNAl(clicked()),this,SLOT(digits_numbers());
+    connect(ui->pushButton_7,SIGNAl(clicked()),this,SLOT(digits_numbers());
+     */
 }
 
+void MainWindow::digits_numbers(){
+    QPushButton *button = (QPushButton *)sender();
+}
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
+
+QString MainWindow::on_pushButton_clicked()
+{
+    return "1";
+}
+
+void MainWindow::on_pushButton_0_clicked()
+{
+
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QString>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -15,6 +15,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    QString on_pushButton_clicked();
+
+    void on_pushButton_0_clicked();
+    void digits_numbers();
 private:
     Ui::MainWindow *ui;
 };
