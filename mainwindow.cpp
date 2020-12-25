7728f0660b04ebddf31cba0ad0e1eef497623e40
Anhelina0224 <63913185+Anhelina0224@users.noreply.github.com>
2020-12-26 01:38:35 +0200

    Interface

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,32 +7,60 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-   /* connect(ui->pushButton_0,SIGNAl(clicked()),this,SLOT(digits_numbers());
-    connect(ui->pushButton_1,SIGNAl(clicked()),this,SLOT(digits_numbers());
-    connect(ui->pushButton_2,SIGNAl(clicked()),this,SLOT(digits_numbers());
-    connect(ui->pushButton_3,SIGNAl(clicked()),this,SLOT(digits_numbers());
-    connect(ui->pushButton_4,SIGNAl(clicked()),this,SLOT(digits_numbers());
-    connect(ui->pushButton_5,SIGNAl(clicked()),this,SLOT(digits_numbers());
-    connect(ui->pushButton_6,SIGNAl(clicked()),this,SLOT(digits_numbers());
-    connect(ui->pushButton_7,SIGNAl(clicked()),this,SLOT(digits_numbers());
-     */
+    QObject::connect(ui->pushButton_0,SIGNAL(clicked()),this,SLOT(digits_numbers()));
+    connect(ui->pushButton_1,SIGNAL(clicked()),this,SLOT(digits_numbers()));
+    connect(ui->pushButton_2,SIGNAL(clicked()),this,SLOT(digits_numbers()));
+    connect(ui->pushButton_3,SIGNAL(clicked()),this,SLOT(digits_numbers()));
+    connect(ui->pushButton_4,SIGNAL(clicked()),this,SLOT(digits_numbers()));
+    connect(ui->pushButton_5,SIGNAL(clicked()),this,SLOT(digits_numbers()));
+    connect(ui->pushButton_6,SIGNAL(clicked()),this,SLOT(digits_numbers()));
+    connect(ui->pushButton_7,SIGNAL(clicked()),this,SLOT(digits_numbers()));
+    connect(ui->pushButton_8,SIGNAL(clicked()),this,SLOT(digits_numbers()));
+    connect(ui->pushButton_percent,SIGNAL(clicked()),this,SLOT(operations()));
+    connect(ui->pushButton_plus_minus,SIGNAL(clicked()),this,SLOT(operations()));
+
 }
 
 void MainWindow::digits_numbers(){
     QPushButton *button = (QPushButton *)sender();
+    double num;
+    QString label;
+    num = (ui->result_show->text()+ button->text()).toDouble();
+    label = QString::number(num, 'g',15);
+    ui->result_show->setText(label );
 }
+void MainWindow::operations(){
+    QPushButton *button = (QPushButton *)sender();
+    double num;
+    QString label;
+
+    if(button->text() == "+/-"){
+    num = ui->result_show->text().toDouble();
+    num *= -1;
+    label = QString::number(num, 'g',15);
+    ui->result_show->setText(label );
+    }
+    else if(button->text() == "%"){
+    num = ui->result_show->text().toDouble();
+    num *= 0.01;
+    label = QString::number(num, 'g',15);
+    ui->result_show->setText(label );
+    }
+}
+
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
 
-QString MainWindow::on_pushButton_clicked()
+void MainWindow::on_pushButton_dot_clicked()
 {
-    return "1";
+    if(!(ui->result_show->text().contains('.')))
+    ui->result_show->setText(ui->result_show->text()+ ".");
 }
 
-void MainWindow::on_pushButton_0_clicked()
+void MainWindow::on_pushButton_Erase_clicked()
 {
 
 }
