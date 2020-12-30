b85ad8c93e9f127a0a0e55d303e5128582ec9574
Anhelina0224 <63913185+Anhelina0224@users.noreply.github.com>
2020-12-30 23:44:57 +0200

    Interface

--- mainwindow.h
+++ mainwindow.h
@@ -4,7 +4,7 @@
 #include <QMainWindow>
 #include <QString>
 #include "calcprog.h"
-
+#include "calcbasic.h"
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -15,7 +15,8 @@ class MainWindow : public QMainWindow
 
 public :
      explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+     ~MainWindow();
+
 
 private slots:
   
@@ -23,12 +24,18 @@ private slots:
     void on_pushButton_dot_clicked();
 
     void on_pushButton_Erase_clicked();
+    void algebraOperations();
     void operations();
-
     void on_another_wnd_clicked();
 
+    void result();
+
 private:
+    double m_firstNum = 0;
+    QString m_operator = " ";
+    double m_secondNum = 0;
     Ui::MainWindow *ui;
     CalcProg *wnd_cacl;
+    CalcBasic *base_calc;
 };
 #endif // MAINWINDOW_H
